c2956a52f5f32b20efe39e4bcaae149591f2c5b2
alonyado <alonyado@gmail.com>
2016-11-11 08:38:25 +0200

    update csvAnalyzer to support Quotes inside csv

--- EEG/LoadDataNew/csvAnalyzer.py
+++ EEG/LoadDataNew/csvAnalyzer.py
@@ -1,6 +1,33 @@
 import re, sys, os
 
-def analyzeCsv(csvContent, delimiter = ',', manipulationFunc = None, additionalConstFields = None):
+def splitLine(line, delimiter, supportQ = True):
+    currMatch = 0
+    escaping = False
+    qCnt = False
+    bufferStr = ''
+
+    tokens = list()
+    for ch in line:
+        if ch == '"' and not(escaping):
+            qCnt = not(qCnt)
+            
+        escaping = (ch =='\\')
+        
+        if ch == delimiter[currMatch] and (not(qCnt) or not(supportQ)):
+            currMatch = currMatch + 1
+            if currMatch >= len(delimiter):
+                currMatch = 0
+                tokens.append(bufferStr)
+                bufferStr = ''
+        else:
+            bufferStr = bufferStr + ch
+
+    if len(bufferStr) > 0:
+        tokens.append(bufferStr)
+
+    return tokens
+            
+def analyzeCsv(csvContent, delimiter = ',', supportQ = True, manipulationFunc = None, additionalConstFields = None):
     if manipulationFunc <> None and type(manipulationFunc) <> dict:
         raise NameError('passed manipulationFunc which should be dict object')
     if additionalConstFields <> None and type(manipulationFunc) <> dict:
@@ -13,16 +40,18 @@ def analyzeCsv(csvContent, delimiter = ',', manipulationFunc = None, additionalC
     header = map(lambda x: x.strip().strip('"').lower() ,header.split(delimiter))
     
     fieldCnt = len(header)
-    tokenSpliter = re.compile(delimiter)
     allLines = allLines[1:len(allLines)]    
-    
+
+    lineNum = 1
     for line in allLines:
-        allTokens = tokenSpliter.split(line)
+        lineNum = lineNum + 1
+        allTokens = splitLine(line, delimiter, supportQ)
         allTokens = map(lambda t: t.strip().strip('"') ,allTokens)
         d = dict()
         if len(allTokens) <> fieldCnt:
             sys.stderr.write('%s\n'%line)
-            raise NameError('Csv Format Exception')
+            raise NameError('Csv Format Exception in line %d'%lineNum)
+            
         for i in xrange(0, fieldCnt):
             if len(header[i]) == 0:
                 continue
@@ -38,10 +67,11 @@ def analyzeCsv(csvContent, delimiter = ',', manipulationFunc = None, additionalC
         
     header = filter(lambda x: len(x) > 0 ,header)    
     return res, header
-def analyzeFile(fpath, delimiter = ',', manipulationFunc = None, additionalConstFields = None):
+
+def analyzeFile(fpath, delimiter = ',', supportQ = True, manipulationFunc = None, additionalConstFields = None):
     if not(os.path.exists(fpath)):
         raise NameError('Path not exist')
     f = open(fpath, 'r')
     data = f.read()
     f.close()
-    return analyzeCsv(data, delimiter, manipulationFunc, additionalConstFields)
+    return analyzeCsv(data, delimiter, supportQ, manipulationFunc, additionalConstFields)
